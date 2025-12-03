c9e650267a551eb8dd6d2235920e8644ad76f915
PrestonCunningham <46457131+PrestonCunningham@users.noreply.github.com>
2025-12-03 14:57:59 -0900

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,1 +1,13 @@
+import mysql.connector
 
+def get_connection():
+    """
+    Returns a connection to the SFILS MySQL database.
+    """
+    return mysql.connector.connect(
+        host="127.0.0.1",
+        user="sfils_user",     # <-- change if needed
+        password="sfils_pw",   # <-- change if needed
+        database="sfils"
+    )
+  
